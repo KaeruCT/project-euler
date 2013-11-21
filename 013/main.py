c9fc847e0f2ba542119b484706d08bcc1b6c09d5
Andres V <darklink898@gmail.com>
2013-11-21 15:12:26 -0600

    typo!

--- 013/main.py
+++ 013/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 f = open('numbers.txt', 'r')
-numbers = [int(i) for i in .readlines()]
+numbers = [int(i) for i in f.readlines()]
 result = str(sum(numbers))[:10]
 
 print result
